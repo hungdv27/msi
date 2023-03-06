098c1accd050c7fdbdb3bbbae1e4c14cbe2d0d59
DESKTOP-7KR0HP7\Cong <minhcongvghy@gmail.com>
2023-03-06 17:07:04 +0700

    validate date, exception

--- src/main/java/com/example/msi/service/SemesterService.java
+++ src/main/java/com/example/msi/service/SemesterService.java
@@ -1,6 +1,7 @@
 package com.example.msi.service;
 
 import com.example.msi.domains.Semester;
+import com.example.msi.exceptions.MSIException;
 import com.example.msi.models.company.CreateSemesterDTO;
 import com.example.msi.models.company.UpdateSemesterDTO;
 import lombok.NonNull;
@@ -8,11 +9,11 @@ import lombok.NonNull;
 import java.util.List;
 
 public interface SemesterService {
-  List<Semester> getAllSemester();
+  List<Semester> getAllSemester() throws MSIException;;
 
-  void addSemester(CreateSemesterDTO semester);
+  void addSemester(CreateSemesterDTO semester) throws MSIException;
 
-  void updateSemester(@NonNull UpdateSemesterDTO payload);
+  void updateSemester(@NonNull UpdateSemesterDTO payload) throws MSIException;
 
-  void deleteSemester(int id);
+  void deleteSemester(int id) throws MSIException;;
 }
--- src/main/java/com/example/msi/service/impl/SemesterServiceImpl.java
+++ src/main/java/com/example/msi/service/impl/SemesterServiceImpl.java
@@ -1,6 +1,8 @@
 package com.example.msi.service.impl;
 
 import com.example.msi.domains.Semester;
+import com.example.msi.exceptions.ExceptionUtils;
+import com.example.msi.exceptions.MSIException;
 import com.example.msi.models.company.CreateSemesterDTO;
 import com.example.msi.models.company.UpdateSemesterDTO;
 import com.example.msi.repository.SemesterRepository;
@@ -9,6 +11,7 @@ import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Service
@@ -16,27 +19,45 @@ import java.util.List;
 public class SemesterServiceImpl implements SemesterService {
   private final SemesterRepository repository;
   @Override
-  public List<Semester> getAllSemester() {
+  public List<Semester> getAllSemester() throws MSIException {
     return repository.findAll();
   }
 
   @Override
-  public void addSemester(CreateSemesterDTO semester) {
-    var entity = Semester.getInstance(semester);
-    repository.save(entity);
-  }
-
-  @Override
-  public void updateSemester(@NonNull UpdateSemesterDTO payload) {
-    var id=payload.getId();
-    repository.findById(id).ifPresent(entity -> {
-      entity.update(payload);
+  public void addSemester(CreateSemesterDTO semester) throws MSIException{
+    if (validateDate(semester.getStartDate(), semester.getEndDate())){
+      var entity = Semester.getInstance(semester);
       repository.save(entity);
-    });
+    } else {
+      throw new MSIException(
+          ExceptionUtils.END_DATE_IS_NOT_BEFORE_OR_EQUAL_START_DATE,
+          ExceptionUtils.messages.get(ExceptionUtils.END_DATE_IS_NOT_BEFORE_OR_EQUAL_START_DATE));
+    }
   }
 
   @Override
-  public void deleteSemester(int id) {
+  public void updateSemester(@NonNull UpdateSemesterDTO payload) throws MSIException {
+    if (validateDate(payload.getStartDate(), payload.getEndDate())){
+      var id=payload.getId();
+      repository.findById(id).ifPresent(entity -> {
+        entity.update(payload);
+        repository.save(entity);
+      });
+    } else {
+      throw new MSIException(
+          ExceptionUtils.END_DATE_IS_NOT_BEFORE_OR_EQUAL_START_DATE,
+          ExceptionUtils.messages.get(ExceptionUtils.END_DATE_IS_NOT_BEFORE_OR_EQUAL_START_DATE));
+    }
+  }
+
+  @Override
+  public void deleteSemester(int id) throws MSIException {
     repository.deleteById(id);
+  }
+
+  private boolean validateDate(String startDate, String endDate){
+    var start = LocalDate.parse(startDate);
+    var end = LocalDate.parse(endDate);
+    return !end.isBefore(start) && !end.isEqual(start);
   }
 }
