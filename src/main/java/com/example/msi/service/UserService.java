0029c00de5e7911ec011cb82858ba73a8c0d4712
Vietanh1311 <83020392+Vietanh1311@users.noreply.github.com>
2023-03-14 16:14:11 +0700

    update

--- src/main/java/com/example/msi/service/UserService.java
+++ src/main/java/com/example/msi/service/UserService.java
@@ -1,9 +1,11 @@
 package com.example.msi.service;
 
+import com.example.msi.domains.User;
 import com.example.msi.models.user.CreateUserDTO;
 import com.example.msi.respone.Data;
 
 import javax.mail.MessagingException;
+import java.util.Optional;
 
 public interface UserService {
   Data register(CreateUserDTO userRegister, StringBuffer siteURL) throws IllegalAccessException, MessagingException;
@@ -13,5 +15,7 @@ public interface UserService {
   Data updatePassword(int userId, String password, String newPassword);
 
   Data forgotPassword(String mail) throws MessagingException;
+
+  Optional<User> userAccessInformation();
 
 }
