2208c01a54f56725ec649e43953c86c053c4b2c9
DESKTOP-7KR0HP7\Cong <minhcongvghy@gmail.com>
2023-03-15 09:32:01 +0700

    import excel company

--- src/main/java/com/example/msi/shared/Constant.java
+++ src/main/java/com/example/msi/shared/Constant.java
@@ -1,7 +1,22 @@
 package com.example.msi.shared;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Constant {
   public static final int DEFAULT_PAGE_NUMBER = 0;
   public static final int DEFAULT_PAGE_SIZE = 10;
   public static final String ID = "id";
+  public static final List<String> INCOME_COMPANY_IMPORT_HEADER =
+      Arrays.asList(
+          Constant.INCOME_COMPANY_TEMPLATE_NAME,
+          Constant.INCOME_COMPANY_TEMPLATE_PHONE_NUMBER,
+          Constant.INCOME_COMPANY_TEMPLATE_EMAIL,
+          Constant.INCOME_COMPANY_TEMPLATE_ADDRESS);
+  public static final String INCOME_COMPANY_TEMPLATE_NAME = "Tên công ty";
+  public static final String INCOME_COMPANY_TEMPLATE_PHONE_NUMBER = "Số điện thoại";
+  public static final String INCOME_COMPANY_TEMPLATE_EMAIL = "Email";
+  public static final String INCOME_COMPANY_TEMPLATE_ADDRESS = "Địa chỉ";
+  public static final String INCOME_COMPANY_TEMPLATE_STATUS = "Status";
+  public static final String F_DUPLICATE = "F_DUPLICATE";
 }
