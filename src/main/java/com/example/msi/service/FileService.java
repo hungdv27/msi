21a31a455afdc075279b916030a0d182194eceb1
Vietanh1311 <83020392+Vietanh1311@users.noreply.github.com>
2023-03-23 13:40:53 +0700

    update

--- src/main/java/com/example/msi/service/FileService.java
+++ src/main/java/com/example/msi/service/FileService.java
@@ -1,9 +1,20 @@
 package com.example.msi.service;
 
+import com.example.msi.domains.FileE;
 import com.example.msi.models.file.CreateFileDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestParam;
 import com.example.msi.response.Data;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
 public interface FileService {
   String uploadFile(MultipartFile multipartFile);
+
+  ResponseEntity<byte[]> downloadFile(String fileName) throws IOException;
+
+  List<FileE> uploadFiles(List<MultipartFile> files) throws IOException;
 }
--- src/main/java/com/example/msi/service/impl/FileServiceImpl.java
+++ src/main/java/com/example/msi/service/impl/FileServiceImpl.java
@@ -1,11 +1,18 @@
 package com.example.msi.service.impl;
 
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.SdkClientException;
 import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.model.S3Object;
 import com.cloudinary.Cloudinary;
 import com.cloudinary.Transformation;
 import com.cloudinary.utils.ObjectUtils;
@@ -17,7 +24,12 @@ import com.example.msi.service.FileService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.PostConstruct;
@@ -25,10 +37,7 @@ import java.io.*;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 
 @Service
 @Slf4j
@@ -88,6 +97,35 @@ public class FileServiceImpl implements FileService {
     return fileUrl;
   }
 
+  @Override
+  public List<FileE> uploadFiles(List<MultipartFile> files) throws IOException {
+    List<FileE> entity = new ArrayList<FileE>();
+    for (MultipartFile file : files) {
+      var newEntity = new FileE();
+      String key = generateFileName(file);
+      ObjectMetadata metadata = new ObjectMetadata();
+      metadata.setContentType(file.getContentType());
+      metadata.setContentLength(file.getSize());
+      PutObjectRequest putObjectRequest = new PutObjectRequest(bucketName, key, file.getInputStream(), metadata);
+      s3Client.putObject(putObjectRequest);
+      newEntity.setFileURL(key);
+      entity.add(repository.save(newEntity));
+    }
+    return entity;
+  }
+
+  @Override
+  public ResponseEntity<byte[]> downloadFile(String fileName) throws IOException {
+    S3Object s3Object = s3Client.getObject(bucketName, fileName);
+    byte[] content = s3Object.getObjectContent().readAllBytes();
+    HttpHeaders headers = new HttpHeaders();
+    headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+    headers.setContentDispositionFormData("attachment", fileName);
+    headers.setContentLength(content.length);
+    ResponseEntity<byte[]> responseEntity = new ResponseEntity<>(content, headers, HttpStatus.OK);
+    s3Object.close();
+    return responseEntity;
+  }
 
 
 }
