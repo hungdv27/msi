a7777b158ad1ecb79992037f6ab80ea37decb8f7
hunglp27 <hung.doviet@lptech.vn>
2023-04-04 22:15:47 +0700

    fix dowload file

--- src/main/java/com/example/msi/controller/FileController.java
+++ src/main/java/com/example/msi/controller/FileController.java
@@ -1,20 +1,13 @@
 package com.example.msi.controller;
 
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.model.S3Object;
 import com.example.msi.domains.FileE;
-import com.example.msi.models.file.CreateFileDTO;
 import com.example.msi.service.FileService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
@@ -35,8 +28,8 @@ public class FileController {
     return service.uploadFiles(files);
   }
 
-  @GetMapping("/download/{fileName}")
-  public ResponseEntity<byte[]> downloadFile(@PathVariable String fileName) throws IOException {
+  @GetMapping("/download")
+  public ResponseEntity<byte[]> downloadFile(@RequestParam String fileName) throws IOException {
     return service.downloadFile(fileName);
   }
 
