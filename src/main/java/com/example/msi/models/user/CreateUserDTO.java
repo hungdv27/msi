ec8748417d751cd96013d35765e9fe48f0ba9963
hungdoviet <hung.doviet@lptech.vn>
2023-04-16 22:15:12 +0700

    update

--- src/main/java/com/example/msi/models/user/CreateUserDTO.java
+++ src/main/java/com/example/msi/models/user/CreateUserDTO.java
@@ -2,11 +2,13 @@ package com.example.msi.models.user;
 
 import com.example.msi.domains.User;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.NonNull;
 
 import java.time.LocalDate;
 
 @Data
+@NoArgsConstructor
 public class CreateUserDTO {
   private String email;
   private String password;
@@ -14,7 +16,7 @@ public class CreateUserDTO {
   private LocalDate dob;
   private String phoneNumber;
 
-  private CreateUserDTO(@NonNull User user){
+  private CreateUserDTO(@NonNull User user) {
     email = user.getEmail();
     password = user.getPassword();
     fullName = user.getFullName();
@@ -22,5 +24,7 @@ public class CreateUserDTO {
     phoneNumber = user.getPhoneNumber();
   }
 
-  public static CreateUserDTO getInstance(@NonNull User entity) { return new CreateUserDTO(entity); }
+  public static CreateUserDTO getInstance(@NonNull User entity) {
+    return new CreateUserDTO(entity);
+  }
 }
