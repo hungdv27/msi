0e9c72e94a5bc507e0f79fe55e31cab8309f00e8
Cương ĐN <cuongdn.sun@gmail.com>
2023-04-20 09:57:22 +0000

    Update UserController.java

--- src/main/java/com/example/msi/controller/UserController.java
+++ src/main/java/com/example/msi/controller/UserController.java
@@ -69,7 +69,7 @@ public class UserController {
       // Trả về jwt cho người dùng.
       CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
       String jwt = tokenProvider.generateToken(userDetails);
-      return ResponseEntity.ok(new Data(new LoginResponse("Bearer " + jwt, new Date((new Date()).getTime() + 30000).getTime(), mapper.map(userDetails.getUser(), LoginUserDTO.class))));
+      return ResponseEntity.ok(new Data(new LoginResponse("Bearer " + jwt, new Date((new Date()).getTime() + 3600000).getTime(), mapper.map(userDetails.getUser(), LoginUserDTO.class))));
     } catch (Exception e) {
       throw new ResponseStatusException(
           HttpStatus.BAD_REQUEST,"Sai thông tin đăng nhập", e
