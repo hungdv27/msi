f913b42ac36eb7b163f77ba4b1a7ca5a5cd4caf7
hungdoviet <hung.doviet@lptech.vn>
2023-04-20 11:41:02 +0700

    update

--- src/main/java/com/example/msi/models/internshipprocess/SearchInternshipProcessDTO.java
+++ src/main/java/com/example/msi/models/internshipprocess/SearchInternshipProcessDTO.java
@@ -2,7 +2,8 @@ package com.example.msi.models.internshipprocess;
 
 import com.example.msi.domains.InternshipProcess;
 import com.example.msi.shared.base.BaseFilter;
-import lombok.*;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.data.jpa.domain.Specification;
 
@@ -15,18 +16,13 @@ import static org.apache.logging.log4j.util.Strings.trimToNull;
 
 @Getter
 @RequiredArgsConstructor
-//@NoArgsConstructor
-//@AllArgsConstructor
-//@Builder
-//@Getter
-//@Setter
 public class SearchInternshipProcessDTO implements BaseFilter<InternshipProcess> {
-  private final String studentCode;
-  private final Integer teacherId;
-  private final Integer semesterId;
-  private final String courseCode;
-  private final Integer page;
-  private final Integer size;
+  private String studentCode;
+  private Integer teacherId;
+  private Integer semesterId;
+  private String courseCode;
+  private Integer page;
+  private Integer size;
 
   public SearchInternshipProcessDTO(String studentCode, Integer teacherId, Integer semesterId, String courseCode) {
     this.studentCode = studentCode;
@@ -37,23 +33,17 @@ public class SearchInternshipProcessDTO implements BaseFilter<InternshipProcess>
     this.size = size();
   }
 
-//private String studentCode;
-//  private Integer teacherId;
-//  private Integer semesterId;
-//  private String courseCode;
-//  private Integer page;
-//  private Integer size;
-
   public String studentCode() {
     return StringUtils.upperCase(trimToNull(studentCode));
   }
+
   public String courseCode() {
     return StringUtils.upperCase(trimToNull(courseCode));
   }
 
   @Override
   public int size() {
-    return (size == null ? 0 : size) < 1 ? 15 : size;
+    return (((size == null) ? 0 : size) < 1) ? 15 : size;
   }
 
   @Override
@@ -70,35 +60,26 @@ public class SearchInternshipProcessDTO implements BaseFilter<InternshipProcess>
                     cb::equal,
                     attr -> root.join("internshipApplication").get(attr).as(String.class),
                     "studentCode",
-                    () -> value
-                )
+                    () -> value)
             ),
-            Optional.ofNullable(semesterId).map(
-                value -> build(
-                    cb::equal,
-                    attr -> root.join("internshipApplication").get(attr).as(Integer.class),
-                    "semesterId",
-                    () -> value
-                )
+            Optional.ofNullable(semesterId).map(value -> build(
+                cb::equal, attr -> root.join("internshipApplication").get(attr).as(Integer.class),
+                "semesterId",
+                () -> value)
             ),
             Optional.ofNullable(courseCode()).map(
                 value -> build(
-                    cb::equal,
-                    attr -> root.join("internshipApplication").get(attr).as(String.class),
+                    cb::equal, attr -> root.join("internshipApplication").get(attr).as(String.class),
                     "courseCode",
-                    () -> value
-                )
+                    () -> value)
             ),
             Optional.ofNullable(teacherId).map(
-                value -> build(
-                    cb::equal,
+                value -> build(cb::equal,
                     attr -> root.get(attr).as(Integer.class),
                     "teacherId",
-                    () -> value
-                )
+                    () -> value)
             )
         ),
-        predicates -> cq.where(predicates).getRestriction()
-    );
+        predicates -> cq.where(predicates).getRestriction());
   }
 }
