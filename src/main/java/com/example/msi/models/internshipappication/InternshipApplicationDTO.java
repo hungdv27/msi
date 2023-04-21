63991fd80117f7c3c4e58ae746e638b390046729
hungdoviet <hung.doviet@lptech.vn>
2023-04-21 23:06:35 +0700

    update

--- src/main/java/com/example/msi/models/internshipappication/InternshipApplicationDTO.java
+++ src/main/java/com/example/msi/models/internshipappication/InternshipApplicationDTO.java
@@ -16,26 +16,29 @@ import static com.example.msi.shared.utils.ServiceUtils.getStudentService;
 
 @Getter
 public class InternshipApplicationDTO {
-  private int id;
-  private StudentDetailDTO student;
-  private InternshipApplicationStatus status;
+  private final int id;
+  private final StudentDetailDTO student;
+  private final InternshipApplicationStatus status;
+  private final int semesterId;
+  private final String note;
+  private final LocalDateTime createdDate;
+  private final LocalDateTime updatedDate;
+  private final String courseCode;
+  private final String instructor;
+  private final String instructorContact;
+  private final String description;
+  private final LocalDate startDate;
+  private final LocalDate endDate;
+  private final Integer totalDayPerWeek;
+  private final Integer totalHourPerShift;
   private CompanyDTO company;
-  private int semesterId;
-  private String note;
-  private LocalDateTime createdDate;
-  private LocalDateTime updatedDate;
-  private String courseCode;
-  private String instructor;
-  private String instructorContact;
-  private String description;
-  private LocalDate startDate;
-  private LocalDate endDate;
-  private Integer totalDayPerWeek;
-  private Integer totalHourPerShift;
 
   private InternshipApplicationDTO(@NonNull InternshipApplication entity) {
     id = entity.getId();
-    var stu = getStudentService().findByCode(entity.getStudentCode()).orElse(null);
+    var stuOptional = getStudentService().findByCode(entity.getStudentCode());
+    if (stuOptional.isEmpty())
+      throw new RuntimeException("Không tìm thấy dữ liệu sinh viên " + entity.getStudentCode());
+    var stu = stuOptional.get();
     student = new StudentDetailDTO(stu);
     status = entity.getStatus();
     semesterId = entity.getSemesterId();
@@ -50,9 +53,7 @@ public class InternshipApplicationDTO {
     endDate = entity.getEndDate();
     totalDayPerWeek = entity.getTotalDayPerWeek();
     totalHourPerShift = entity.getTotalHourPerShift();
-    Optional.ofNullable(entity.getCompanyId()).ifPresent(
-        value -> company = CompanyDTO.getInstance(getCompanyService().getCompanyById(entity.getCompanyId()))
-    );
+    Optional.ofNullable(entity.getCompanyId()).ifPresent(value -> company = CompanyDTO.getInstance(getCompanyService().getCompanyById(entity.getCompanyId())));
   }
 
   public static InternshipApplicationDTO getInstance(@NonNull InternshipApplication entity) {
